340b3ef8b5c8c260667677f2a6effbdd167d9f25
sho6715 <sho.hedwig.s@gmail.com>
2019-05-30 16:14:49 +0900

    AEGIS更新test

--- newSKM2/newSKM2.c
+++ newSKM2/newSKM2.c
@@ -345,6 +345,9 @@ PRIVATE void MODE_exe( void )
 				TIME_wait(50);
 			}
 */
+
+//test　githubのテスト更新
+
 //			TIME_wait(200);
 			MOT_setTrgtSpeed(SEARCH_SPEED);
 			MOT_setSuraStaSpeed( (FLOAT)SEARCH_SPEED );							// スラローム開始速度設定
