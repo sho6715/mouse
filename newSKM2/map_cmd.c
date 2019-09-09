81699427ed4a919eed1ec1789301dd6fd89d0269
sho6715 <sho.hedwig.s@gmail.com>
2019-09-09 17:42:20 +0900

    20190909_skewcmd_fix

--- newSKM2/map_cmd.c
+++ newSKM2/map_cmd.c
@@ -612,24 +612,38 @@ PUBLIC void MAP_makeSkewCmdList( void )
 		c4 = scom_temp[ct_st+3];
 
 		//	直進 → 右45度 → 斜め
-		if( (c1<=GO32) && (c2==R90S) && (c3==L90S) && (ct_st != 0) )
+		if( (c1<=GO32) && (c2==R90S) && (c3==L90S) )
 		{
-			if( c1-1 != 0 ) tcom[ ct_n++ ] = c1 - 1;		//	前の複合コマンドによって直線区間が消えない場合
-			tcom[ ct_n++ ] = RS45N;
-			ct_st ++;
+			if( c1-1 != 0 ) {
+				tcom[ ct_n++ ] = c1 - 1;		//	前の複合コマンドによって直線区間が消えない場合
+				tcom[ ct_n++ ] = RS45N;
+				ct_st ++;
 
-			x = (USHORT)(NGO1 - 1);		//	斜めモード
-			flag = 0;
+				x = (USHORT)(NGO1 - 1);		//	斜めモード
+				flag = 0;
+			}
+			else{
+				tcom[ ct_n ] = scom_temp[ct_st];
+				ct_st ++;
+				ct_n ++;
+			}
 		}
 		//	直進 → 左45度 → 斜め
-		else if( (c1<=GO32) && (c2==L90S) && (c3==R90S) && (ct_st != 0) )
+		else if( (c1<=GO32) && (c2==L90S) && (c3==R90S) )
 		{
-			if( c1-1 != 0 ) tcom[ ct_n++ ] = c1 - 1;		//	前の複合コマンドによって直線区間が消えない場合
-			tcom[ ct_n++ ] = LS45N;
-			ct_st ++;
+			if( c1-1 != 0 ){ 
+				tcom[ ct_n++ ] = c1 - 1;		//	前の複合コマンドによって直線区間が消えない場合
+				tcom[ ct_n++ ] = LS45N;
+				ct_st ++;
 
-			x = (USHORT)(NGO1 - 1);		//	斜めモード
-			flag = 0;
+				x = (USHORT)(NGO1 - 1);		//	斜めモード
+				flag = 0;
+			}
+			else{
+				tcom[ ct_n ] = scom_temp[ct_st];
+				ct_st ++;
+				ct_n ++;
+			}
 		}
 
 		//	直進 → 右90度 → 直進
